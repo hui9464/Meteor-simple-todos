654df512117ed0fb67906f5be62931c42c3e4747
hui9464 <495287822@qq.com>
2017-08-10 14:41:03 +0800

    上传代码
    meteor简单项目

---
+++ imports/ui/task.js
@@ -0,0 +1,29 @@
+import { Template } from 'meteor/templating';
+
+import { Tasks } from '../api/tasks.js';
+
+import './task.html';
+
+Template.task.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
+  },
+});
+
+Template.task.events({
+  'click .toggle-checked'() {
+    console.log(this._id);
+
+    // Set the checked property to the opposite of its current value
+    Meteor.call('tasks.setChecked', this._id, !this.checked);
+  },
+
+  'click .delete'() {
+    Meteor.call('tasks.remove', this._id);
+  },
+
+  'click .toggle-private'() {
+    console.log('private: ' + !this.private);
+    Meteor.call('tasks.setPrivate', this._id, !this.private);
+  },
+});
